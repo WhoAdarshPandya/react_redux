2b037f760d96900a8ae9ad68c6650c829d984f06
WhoAdarshPandya <adarshpandya0@gmail.com>
2020-05-21 17:10:05 +0530

    deploy readt

---
+++ build/precache-manifest.6828969d4287c4c75299a2793c040bb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd60063211875c3cd4cb85de2a63208",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4533d1c8cfdf963eae53",
+    "url": "/static/js/2.dc51a776.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dc51a776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33e985ea49d3d5887448",
+    "url": "/static/js/main.98d2452f.chunk.js"
+  },
+  {
+    "revision": "01e52f0bc1c10e5865cd",
+    "url": "/static/js/runtime-main.a9dfdfad.js"
+  }
+]);
